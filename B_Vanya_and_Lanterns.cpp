bb1f6f6915a70b3c05a2c01b93bb0bc8fb423718
hippie <paramdeep@live.com>
2020-09-08 15:24:34 -0500

    Refactored solution for CF#492 B

--- B_Vanya_and_Lanterns.cpp
+++ B_Vanya_and_Lanterns.cpp
@@ -2,19 +2,8 @@
 
 using namespace std;
 
-const int N = 1009;
-int lanternPosition[N];
-
-int main() {
-	int totalLanterns, streetLength;
-	scanf("%d %d", &totalLanterns, &streetLength);	
-
-	for (int i = 0; i < totalLanterns; i ++) {
-		scanf("%d", &lanternPosition[i]);
-	}
-
-	sort(lanternPosition, lanternPosition + totalLanterns);
-
+double getMaxCoverageRequired(vector<int> &lanternPosition, int streetLength) {
+	int totalLanterns = lanternPosition.size();
 	double maxCoverageRequired = lanternPosition[0] - 0;
 	for (int i = 1; i < totalLanterns; i ++) {
 		maxCoverageRequired = max(maxCoverageRequired, 
@@ -22,7 +11,23 @@ int main() {
 	}
 	maxCoverageRequired = max(maxCoverageRequired, 
 		(double) streetLength - lanternPosition[totalLanterns - 1]);
+	return maxCoverageRequired;
+}
 
+int main() {
+	int totalLanterns, streetLength;
+	scanf("%d %d", &totalLanterns, &streetLength);	
+
+	vector<int> lanternPosition(totalLanterns);
+	for (int i = 0; i < totalLanterns; i ++) {
+		scanf("%d", &lanternPosition[i]);
+	}
+	sort(lanternPosition.begin(), lanternPosition.end());
+
+	double maxCoverageRequired = getMaxCoverageRequired(
+		lanternPosition, 
+		streetLength
+	);
 
 	printf("%.*f", 10, maxCoverageRequired);
 
